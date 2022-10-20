34309eef5d5bbe9a9096abeed61db5726fa63573
RiazK <RiazCKhan@gmail.com>
2022-10-20 18:28:01 -0400

    add image meta data table script

--- feedapp-db/codingRoomsEx.sql
+++ feedapp-db/codingRoomsEx.sql
@@ -50,3 +50,16 @@ ALTER TABLE "Post"
 ADD CONSTRAINT "username_fkey" 
 FOREIGN KEY ("usernameKey") 
 REFERENCES "UserID" ("username");
+
+-- IMAGEMETADATA
+
+DROP TABLE IF EXISTS "ImageMetaData" CASCADE;
+CREATE TABLE  "ImageMetaData" (
+  "imageID" varchar(255) NOT NULL,
+  "imageName" varchar(255) NOT NULL,
+  "imageSize" varchar(255),
+  "imageFormat" varchar(255),
+  "imageDate" varchar(255),
+  "resolution" varchar(255),
+  "postKey" varchar(255)
+);
