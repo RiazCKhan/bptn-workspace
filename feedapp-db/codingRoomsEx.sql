bf459a5de74c969b64c4bcc53e9fd5da264a6c9e
RiazK <RiazCKhan@gmail.com>
2022-10-20 18:32:54 -0400

    add seeds for imagemetadata table

--- feedapp-db/codingRoomsEx.sql
+++ feedapp-db/codingRoomsEx.sql
@@ -63,3 +63,9 @@ CREATE TABLE  "ImageMetaData" (
   "resolution" varchar(255),
   "postKey" varchar(255)
 );
+
+INSERT INTO "ImageMetaData" ("imageID", "imageName", "imageSize", "imageFormat", "imageDate", "resolution", "postKey") 
+VALUES ('01', 'OW', '200mb', 'png', '01/01/0001', '400x400', '123abc');
+
+INSERT INTO "ImageMetaData" ("imageID", "imageName", "imageSize", "imageFormat", "imageDate", "resolution", "postKey") 
+VALUES ('02', 'OW2', '400mb', 'jpeg', '02/02/0002', '600x400', '456def');
