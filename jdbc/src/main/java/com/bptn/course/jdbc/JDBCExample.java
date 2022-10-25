101af29cee442ab14f80cb46aafaeb12cc936bc5
RiazK <RiazCKhan@gmail.com>
2022-10-25 14:14:42 -0400

    add professor examples for preparedStatements and Insert statements

--- jdbc/src/main/java/com/bptn/course/jdbc/JDBCExample.java
+++ jdbc/src/main/java/com/bptn/course/jdbc/JDBCExample.java
@@ -1,10 +1,11 @@
 package com.bptn.course.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+import java.sql.*;
 
 public class JDBCExample {
 
