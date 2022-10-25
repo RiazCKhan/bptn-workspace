39b3b7f6b460eeb75431c49018daa7cbb92cb208
RiazK <RiazCKhan@gmail.com>
2022-10-25 13:20:24 -0400

    add query for UserID table

--- jdbc/src/main/java/com/bptn/course/jdbc/JDBCExample.java
+++ jdbc/src/main/java/com/bptn/course/jdbc/JDBCExample.java
@@ -2,7 +2,9 @@ package com.bptn.course.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCExample {
 
@@ -21,15 +23,23 @@ public class JDBCExample {
 
 		String connStr = "jdbc:postgresql://" + dbUrl + ":" + port + "/" + dbName;
 
-		try (Connection conn = DriverManager.getConnection(connStr, username, password)) {
-			
+		String sqlParam = "SELECT * FROM \"UserID\"";
+
+		try (Connection conn = DriverManager.getConnection(connStr, username, password);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sqlParam);) {
+
 			System.out.println("Connection Opened Successful");
+
+			while (rs.next()) {
+				System.out.println("Users: " + rs.getString("username"));
+			}
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 
 		}
-		
+
 //		finally {
 //
 //			try {
